10cb4381a0d9cded0c9354828e3e1c60fd468774
Matthew Mckenzie <matthew.m.mckenzie@gmail.com>
2012-01-16 17:17:22 +1000

    HA, bitmap to texture ftw works

--- Illisian.Niva.Axiom/RenderItems/CubeBrowser.cs
+++ Illisian.Niva.Axiom/RenderItems/CubeBrowser.cs
@@ -8,6 +8,7 @@ using Axiom.Media;
 using System.Drawing.Imaging;
 using System.IO;
 using Axiom.Math;
+using System.Drawing;
 
 namespace Illisian.Niva.AxiomEngine
 {
@@ -39,38 +40,54 @@ namespace Illisian.Niva.AxiomEngine
 			_sceneNode.Yaw(45);
 			_sceneNode.Position = new Vector3(0, 0, -300);
 
+			Bitmap bmp = new Bitmap(100, 100, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+			using (Graphics gImage = Graphics.FromImage(bmp))
+			{
+				gImage.FillRectangle(Brushes.Red, 0, 0, bmp.Width, bmp.Height);
+				gImage.DrawRectangle(Pens.Black, 10, 10, bmp.Width - 20, bmp.Height - 20);
+			}
+
 			TextureUtil.CreateDynamicTextureAndMaterial(
 				"DynamicTexture", 
 				"DynamicMaterial", 
-				_browserHeight, 
-				_browserWidth, 
+				100, 
+				100, 
 				out _texture, 
 				out _material);
 
+			bmp.Save(@"E:\Temp\test2.jpg", ImageFormat.Jpeg);
 
-			_entity.MaterialName = "Examples/TestImage";
-			_entity.CastShadows = true;
+			TextureUtil.BitmapToTexture(bmp, _texture);
+			
+			bmp.Save(@"E:\Temp\test3.jpg", ImageFormat.Jpeg);
 
-			Core.BrowserManager.BrowserRenderEvent += new BrowserRenderEventHandler(BrowserManager_BrowserRenderEvent);
-			_browserId = Core.BrowserManager.CreateBrowser("http://www.google.com.au", _browserWidth, _browserHeight);
+
+			_entity.MaterialName = "DynamicMaterial";
+
+			//_entity.MaterialName = "Examples/TestImage";
+			//_entity.CastShadows = true;
+
+			//Core.BrowserManager.BrowserRenderEvent += new BrowserRenderEventHandler(BrowserManager_BrowserRenderEvent);
+			//_browserId = Core.BrowserManager.CreateBrowser("http://www.google.com.au", _browserWidth, _browserHeight);
 		}
 
-		void BrowserManager_BrowserRenderEvent(Guid id, System.Drawing.Bitmap screen)
-		{
-			if (id == _browserId)
-			{
+		//void BrowserManager_BrowserRenderEvent(Guid id, System.Drawing.Bitmap screen)
+		//{
+		//    if (id == _browserId)
+		//    {
 
-				_entity.MaterialName = "DynamicMaterial";
-				TextureUtil.BitmapToTexture(screen, _texture);
-				//_texture.Reload();
-				//_material.Reload();
-
-
-
-			}
+		//    //	_entity.MaterialName = "DynamicMaterial";
+		//        //TextureUtil.BitmapToTexture(screen, _texture);
+		//        //_texture.Reload();
+		//        //_material.Reload();
 
 
-		}
+
+		//    }
+
+
+		//}
 
 
 		public void OnRenderFrame(long timeDelta)
--- Illisian.Niva.Axiom/TextureUtil.cs
+++ Illisian.Niva.Axiom/TextureUtil.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using Axiom.Graphics;
 using System.Drawing.Imaging;
 using System.IO;
+using Axiom.Media;
 
 namespace Illisian.Niva.AxiomEngine
 {
@@ -17,41 +18,75 @@ namespace Illisian.Niva.AxiomEngine
 		/// </summary>
 		/// <param name="bmp">The BMP.</param>
 		/// <param name="texture">The texture.</param>
-		public static void BitmapToTexture(Bitmap bitmap, Texture texture)
+		public unsafe static void BitmapToTexture(Bitmap bitmap, Texture texture)
 		{
 
-			var bmp = bitmap.Clone(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height), PixelFormat.Format32bppArgb);
+			var bmp = bitmap.Clone(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
 			var bitmapData = bmp.LockBits(
 				new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height),
 				ImageLockMode.ReadOnly,
 				bmp.PixelFormat);
 
-			var buffer = texture.GetBuffer();
+			HardwarePixelBuffer buffer = texture.GetBuffer();
 			buffer.Lock(Axiom.Graphics.BufferLocking.Discard);
+			PixelBox pixelBox = buffer.CurrentLock;
 
+			UInt32* texData = (UInt32*)pixelBox.Data;
+			UInt32* mapData = (UInt32*)bitmapData.Scan0;
 
-			buffer.CurrentLock.Data = bitmapData.Scan0;
-			buffer.CurrentLock.Format = Axiom.Media.PixelFormat.A8R8G8B8;
+			var height = Math.Min(pixelBox.Height, bmp.Height);
+			var width = Math.Min(pixelBox.Width, bmp.Width);
+
+			for (var y = 0; y < height; ++y)
+				for (var x = 0; x < width; ++x)
+					texData[pixelBox.RowPitch * y + x] = mapData[bitmapData.Stride / 4 * y + x];
 
 			buffer.Unlock();
 			bmp.UnlockBits(bitmapData);
-			bmp.Dispose();
-
-			//using (MemoryStream ms = new MemoryStream())
-			//{
-			//    //bmp.
-			//    bmp.Save(ms,ImageFormat.MemoryBmp);
-			//    //bmp.Save(@"E:\Temp\test.jpg", ImageFormat.Jpeg);
-
-			//    texture.LoadRawData(ms, bmp.Width, bmp.Height, Axiom.Media.PixelFormat.A8R8G8B8);
-			//    //texture.LoadImage(Axiom.Media.Image.FromStream(ms, "jpeg"));
-
-			//    //texture.LoadImage(Axiom.Media.Image.FromRawStream(ms, bmp.Width, bmp.Height, Axiom.Media.PixelFormat.A8R8G8B8));
-			//}
-
 
 		}
+
+		/*
+		 * System::Void JointPositionOverlay::copyBitmapToTexture()
+{
+  // Lock the texture buffer so we can write to it
+
+  Ogre::HardwarePixelBufferSharedPtr buffer = mTexture->getBuffer ();
+  buffer->lock(Ogre::HardwareBuffer::HBL_DISCARD);
+  const Ogre::PixelBox &pb = buffer->getCurrentLock();
+  Ogre::uint32 *texData = static_cast<Ogre::uint32*>(pb.data);
+
+
+  // Lock the bitmap buffer so we can read from it
+
+  System::Drawing::Imaging::BitmapData^ bmd = 
+    mBitmap->LockBits(
+      System::Drawing::Rectangle(0, 0, mBitmap->Width, mBitmap->Height),
+      System::Drawing::Imaging::ImageLockMode::ReadOnly, 
+      System::Drawing::Imaging::PixelFormat::Format32bppArgb);
+  Ogre::uint32* mapData = static_cast<Ogre::uint32*>(bmd->Scan0.ToPointer());
+
+
+  // Now copy the data between buffers
+
+  size_t height = std::min (pb.getHeight(), (size_t)bmd->Height);
+  size_t width = std::min(pb.getWidth(), (size_t)bmd->Width);
+  for(size_t y=0; y<height; ++y) 
+    for(size_t x=0; x<width; ++x) 
+      texData[pb.rowPitch*y + x] = mapData[bmd->Stride/4 * y + x];
+
+
+  // Unlock the buffers again
+
+  mBitmap->UnlockBits(bmd);
+  buffer->unlock();
+}
+		 * 
+		 * 
+		 * 
+		 */
+
 		/// <summary>
 		/// Creates the dynamic texture and material.
 		/// </summary>
@@ -61,19 +96,19 @@ namespace Illisian.Niva.AxiomEngine
 		/// <param name="textureWidth">Width of the texture.</param>
 		/// <param name="texture">The texture.</param>
 		/// <param name="material">The material.</param>
-		public static void CreateDynamicTextureAndMaterial(string TextureName, string MaterialName,int textureHeight, int textureWidth, out Texture texture, out Material material)
+		public static void CreateDynamicTextureAndMaterial(string TextureName, string MaterialName, int textureHeight, int textureWidth, out Texture texture, out Material material)
 		{
 			texture = TextureManager.Instance.CreateManual(TextureName,
 						ResourceGroupManager.DefaultResourceGroupName,
 						TextureType.TwoD, textureWidth, textureHeight, 0,
-						Axiom.Media.PixelFormat.A8R8G8B8, TextureUsage.Dynamic);
+						Axiom.Media.PixelFormat.A8R8G8B8, TextureUsage.DynamicWriteOnlyDiscardable);
 
 
 			material = (Material)MaterialManager.Instance.Create(MaterialName,
 						ResourceGroupManager.DefaultResourceGroupName);
 
 			material.GetTechnique(0).GetPass(0).CreateTextureUnitState(TextureName);
-			
+
 
 		}
 
